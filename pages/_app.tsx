127acb68b4723e13a1a86494dc41226393735cd6
ted-han <taekyung28h@gmail.com>
2023-01-12 23:40:56 +0900

    head meat tag 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>HipHip</title>
+        <meta name="description" content="냠냠냠냠냠" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="맛집쓰" key="힙힙" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
