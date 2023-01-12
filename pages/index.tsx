ce92dff2b35579cf8958ea8800ae956efafc574d
ted-han <taekyung28h@gmail.com>
2023-01-12 22:27:44 +0900

    노션이랑 데이터 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect, useRef } from "react";
+import Image from "next/image";
 import styles from "styles/Home.module.css";
 
 export async function getStaticProps() {
-  const res = await fetch(
+  // 노션 DB에서 데이터 가져오기
+  const placeRes = await fetch(
     `https://api.notion.com/v1/databases/${process.env.NOTION_DB}/query`,
     {
       method: "POST",
@@ -13,64 +15,75 @@ export async function getStaticProps() {
       },
     },
   );
-  const posts = await res.json();
-  const dataa = posts.results.map((v) => {
+  const place = await placeRes.json();
+  const places = place.results.map((v) => {
     const name = v.properties?.["이름"]?.title[0]?.plain_text;
     const address = v.properties?.["주소"]?.rich_text[0]?.plain_text;
+    const comment = v.properties?.["한줄평"]?.rich_text[0]?.plain_text;
+    const type = v.properties?.["분류"]?.select?.name === "커피" ? 2 : 1;
+    const imgUrl = v.properties?.["사진"]?.files[0]?.file?.url || "";
     const tags = v.properties?.["태그"]?.multi_select?.map((t) => t.name);
     return {
       name,
       address,
       tags,
+      comment,
+      type,
+      imgUrl,
     };
   });
-  // console.log(dataa);
 
-  // todo: 결과값 lat,lng 구해서 가공
-  // todo: https://api.ncloud-docs.com/docs/ai-naver-mapsgeocoding
+  // 노션 페이지에서 데이터 가져오기
+  const textRes = await fetch(
+    `https://api.notion.com/v1/blocks/${process.env.NOTION_PAGE}/children?page_size=100`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_KEY}`,
+        "Notion-Version": "2022-06-28",
+      },
+    },
+  );
+  const text = await textRes.json();
+  const plainText = text.results[0].paragraph.rich_text[0].plain_text;
+  const geoInfo = JSON.parse(plainText);
 
-  // const resGeo = await fetch(
-  //   `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=서울 서초구 서초대로46길 20-7 1층`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "X-NCP-APIGW-API-KEY-ID": `${process.env.NCP_CLIENT_ID}`,
-  //       "X-NCP-APIGW-API-KEY": `${process.env.NCP_CLIENT_KEY}`,
-  //     },
-  //   },
-  // );
-  // const geocode = await resGeo.json();
-
-  const data = [
-    {
-      name: "동신면가",
-      address: "서울 서초구 반포대로28길 31 1층",
-      tags: ["한식"],
-      lat: 37.491006,
-      lng: 127.010081,
-      type: 1,
-    },
-    {
-      name: "설눈",
-      address: "서울 서초구 서초대로46길 20-7 1층",
-      tags: ["한식", "온반"],
-      lat: 37.491233,
-      lng: 127.009769,
-      type: 1,
-    },
-    {
-      name: "Cafe Z",
-      address: "서울 서초구 서초대로46길 20-7 1층",
-      tags: ["드립커피"],
-      lat: 37.499233,
-      lng: 127.029769,
-      type: 2,
-    },
-  ];
+  // 페이지정보에 lat,lng 있으면 사용, 없으면 naver geocode api로 확인
+  const newPlaces = [];
+  for (let v of places) {
+    if (!v.name) continue;
+    if (geoInfo[v.name]) {
+      const { lat, lng } = geoInfo[v.name];
+      newPlaces.push({
+        ...v,
+        lat,
+        lng,
+      });
+    } else {
+      const resGeo = await fetch(
+        `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=서울 서초구 서초대로46길 20-7 1층`,
+        {
+          method: "GET",
+          headers: {
+            "X-NCP-APIGW-API-KEY-ID": `${process.env.NCP_CLIENT_ID}`,
+            "X-NCP-APIGW-API-KEY": `${process.env.NCP_CLIENT_KEY}`,
+          },
+        },
+      );
+      const geocode = await resGeo.json();
+      const lat = geocode.addresses[0].y;
+      const lng = geocode.addresses[0].x;
+      newPlaces.push({
+        ...v,
+        lat,
+        lng,
+      });
+    }
+  }
 
   return {
     props: {
-      data,
+      data: newPlaces,
     },
   };
 }
@@ -85,8 +98,8 @@ export default function Map({ data }) {
 
   useEffect(() => {
     setMyLocation({
-      lat: 37.491233,
-      lng: 127.010081,
+      lat: "37.491233",
+      lng: "127.010081",
     });
 
     // 테스트할때만 주석
@@ -119,39 +132,13 @@ export default function Map({ data }) {
     });
 
     naver.maps.Event.addListener(mapRef.current, "click", function (e) {
-      console.log(e);
-      setIndex(null);
       onClick(false);
+      setTimeout(() => {
+        setIndex(null);
+      }, 100);
     });
 
     addMarker(0);
-    // data.map((v, i) => {
-    //   const location = new naver.maps.LatLng(v.lat, v.lng);
-    //   const marker = new naver.maps.Marker({
-    //     position: location,
-    //     map: mapRef.current,
-    //   });
-    //   markerRef.current.push({
-    //     type: v.type,
-    //     lat: v.lat,
-    //     lng: v.lng,
-    //     marker: marker,
-    //   });
-    //   naver.maps.Event.addListener(marker, "click", function (e) {
-    //     // todo: 넣을지말지 고민
-    //     // mapRef.current.panTo(location, {
-    //     //   duration: 300,
-    //     // });
-    //     setIndex((prev) => {
-    //       if (prev === i) {
-    //         onClick(false);
-    //         return null;
-    //       }
-    //       onClick(true);
-    //       return i;
-    //     });
-    //   });
-    // });
   }, [data, myLocation]);
 
   const addMarker = (type: number) => {
@@ -185,15 +172,8 @@ export default function Map({ data }) {
     });
   };
 
-  console.log(markerRef.current);
-
   const onClickCategory = (i) => {
     if (categoryIndex === i) return;
-    // console.log(markerRef.current);
-
-    // todo: 마커 삭제할 때 markerRef.current 도 업데이트
-    // todo: 마커 추가할 때 useEffect때 하는거 함수로 떼서 사용
-
     if (categoryIndex === 0) {
       const type = i === 1 ? 2 : 1;
       const newMarkers = markerRef.current.filter((m) => {
@@ -208,14 +188,12 @@ export default function Map({ data }) {
       const type = categoryIndex === 1 ? 2 : 1;
       addMarker(type);
     } else {
-      console.log(markerRef.current);
       markerRef.current.map((m) => {
         m.marker.setMap(null);
       });
       markerRef.current = [];
       addMarker(i);
     }
-
     setCategoryIndex(i);
   };
 
@@ -248,6 +226,17 @@ export default function Map({ data }) {
       </div>
       <div className={styles.bottomSheet} ref={bottomSheetRef}>
         <h3>{data[index]?.name}</h3>
+        <div>
+          <span>{data[index]?.comment}</span>
+        </div>
+        <div>
+          {data[index]?.tags?.map((t) => (
+            <span key={t}>{t}</span>
+          ))}
+        </div>
+        {data[index]?.imgUrl && (
+          <Image src={data[index]?.imgUrl} alt="" width={50} height={50} />
+        )}
       </div>
     </>
   );
